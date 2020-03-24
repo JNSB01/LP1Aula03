7895a3a45226cc543040b63322d58010a7406e1f
João Borges <joaonunos.borges@gmail.com>
2020-03-24 05:37:27 +0000

    Ex1Aula03

---
+++ PercorreString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Escreva uma string");
+            string s = Console.ReadLine();
+            Console.WriteLine("Escreva um caractér");
+            char c = Console.ReadKey().KeyChar;
+            s = s.Replace($"{c}","");
+            Console.WriteLine();
+            Console.WriteLine($"{s}");
+        }
+    }
+}
